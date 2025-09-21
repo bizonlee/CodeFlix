fea644bd0eb2fd270c17d066e37dca528c892d97
konstantin.zhdanov <konstantin.zhdanov@corp.mail.ru>
2025-09-21 03:08:16 +0300

    tracer

--- CodeFlix/CodeFlix/Resources/AppDelegate.swift
+++ CodeFlix/CodeFlix/Resources/AppDelegate.swift
@@ -5,15 +5,33 @@
 //  Created by Zhdanov Konstantin on 06.08.2025.
 //
 
+import OKTracer
+
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var tracerService: TracerServiceProtocol!
 
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        tracerService = TracerFactory.tracerServiceForCrashReporting(EndpointConfiguration(token: "oxvjFwJbRiCgkzDZx66w3KgmoCNgAvaEBgi5qK4x6kI"))
+        tracerService.start()
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // Обработка сбоя приложения после перезапуска
+        switch tracerService.lastSessionState() {
+        case let .normal(crashCount):
+            print("В последней сессии сбоев не было. Не отправлено \(crashCount) сбоев.")
+        case let .crashed(crashCount, result):
+            print("В последней сессии был сбой. Не отправлено \(crashCount) сбоев.")
+            switch result {
+            case let .success(crashModel):
+                print("Время последнего сбоя\(crashModel.timestamp). Время последнего запуска приложения \(crashModel.startTime).")
+            case let .failure(error):
+                print("При получении последнего сбоя произошла ошибка \(error)")
+            }
+        }
         return true
     }
 
@@ -24,5 +42,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
+
+    
 }
+
 
