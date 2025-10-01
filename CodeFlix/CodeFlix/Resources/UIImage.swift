78bb86538523aa5ba8eece119f35380c39416115
a.gryaznoy <a.gryaznoy@corp.mail.ru>
2025-10-02 01:26:39 +0300

    fix main color and add section header

--- CodeFlix/CodeFlix/Resources/UIImage.swift
+++ CodeFlix/CodeFlix/Resources/UIImage.swift
@@ -8,5 +8,5 @@ import UIKit
 
 extension UIImage {
 
-    static let mainTitleBanner = UIImage(named: "MainBanner")
+    
 }
--- CodeFlix/CodeFlix/Scenes/Main/MainVC.swift
+++ CodeFlix/CodeFlix/Scenes/Main/MainVC.swift
@@ -7,16 +7,7 @@ final class MainVC: UIViewController, UICollectionViewDelegate {
     private var responseFilm: [Film] = []
     private var groupedFilms: [[Film]] = []
 
-
-
-    private lazy var titleBanner: UIImageView = {
-        let banner = UIImageView()
-        banner.image = .mainTitleBanner
-        banner.contentMode = .scaleAspectFill
-        return banner
-    }()
-
-    private lazy var compositionalLayout = {
+    private lazy var compositionalLayout: UICollectionViewCompositionalLayout = {
         let itemSize = NSCollectionLayoutSize(widthDimension: .absolute(150), heightDimension: .absolute(250))
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
 
@@ -28,21 +19,33 @@ final class MainVC: UIViewController, UICollectionViewDelegate {
         section.orthogonalScrollingBehavior = .continuous
         section.contentInsets = .init(top: 0, leading: 0, bottom: 0, trailing: 0)
 
+        let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(44))
+        let sectionHeader = NSCollectionLayoutBoundarySupplementaryItem(
+            layoutSize: headerSize,
+            elementKind: UICollectionView.elementKindSectionHeader,
+            alignment: .top
+        )
+        section.boundarySupplementaryItems = [sectionHeader]
+
         let layout = UICollectionViewCompositionalLayout(section: section)
         return layout
     }()
 
-    private lazy var collectionView = {
+    private lazy var collectionView: UICollectionView = {
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: compositionalLayout)
         collectionView.register(FilmItem.self, forCellWithReuseIdentifier: "FilmItem")
+        collectionView.register(GenreHeaderView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "GenreHeader")
         collectionView.dataSource = self
         return collectionView
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView.backgroundColor = .mainBackgroundColor
-        view.backgroundColor = .mainBackgroundColor
+
+        title = "CodeFlix"
+
+        collectionView.backgroundColor = .secondarySystemBackground
+        view.backgroundColor = .secondarySystemBackground
 
         collectionView.delegate = self
 
@@ -50,35 +53,24 @@ final class MainVC: UIViewController, UICollectionViewDelegate {
         viewModel.searchFilmsByGenres(genres: genres)
 
         view.addSubview(collectionView)
-        view.addSubview(titleBanner)
     }
-
-    //MARK: - Setup Subviews
 
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
 
-        titleBanner.frame = .init(
-            origin: .zero,
-            size: .init(
-                width: view.bounds.width,
-                height: 180))
-
-
         collectionView.frame = .init(
             origin: .init(
                 x: 0,
-                y: titleBanner.frame.height + 50),
+                y: 150),
             size: .init(
                 width: view.bounds.width,
-                height: view.bounds.height - titleBanner.frame.height))
+                height: view.bounds.height))
     }
 }
 
-    //MARK: - Setup Data CollectionView
+// MARK: - DataSource & Delegate
 
 extension MainVC: UICollectionViewDataSource {
-
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         groupedFilms.count
     }
@@ -89,9 +81,7 @@ extension MainVC: UICollectionViewDataSource {
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FilmItem", for: indexPath) as! FilmItem
-
         let film = groupedFilms[indexPath.section][indexPath.item]
-
         cell.configure(
             title: film.title,
             imageURL: film.poster?.url ?? ""
@@ -104,13 +94,20 @@ extension MainVC: UICollectionViewDataSource {
         let filmVC = FilmVC(film: film)
         navigationController?.pushViewController(filmVC, animated: true)
     }
+
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        if kind == UICollectionView.elementKindSectionHeader {
+            let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "GenreHeader", for: indexPath) as! GenreHeaderView
+            header.titleLabel.text = genres[indexPath.section].capitalized
+            return header
+        }
+        return UICollectionReusableView()
+    }
 }
 
 extension MainVC {
-
     private func sortFilms(films: [Film], by genres: [String]) {
         var grouped: [[Film]] = []
-
         for genre in genres {
             let filmsForGenre = films.filter { film in
                 guard let filmGenres = film.genres else { return false }
@@ -126,12 +123,28 @@ extension MainVC: SearchViewModelDelegate {
     func showErrorAlert(message: String) {
         showErrorAlertController(message: message)
     }
-
     func updateUI(with films: [Film]) {
         DispatchQueue.main.async {
             self.responseFilm = films
             self.sortFilms(films: films, by: self.genres)
             self.collectionView.reloadData()
         }
+    }
+}
+
+class GenreHeaderView: UICollectionReusableView {
+    let titleLabel = UILabel()
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        titleLabel.font = .boldSystemFont(ofSize: 24)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(titleLabel)
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            titleLabel.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
